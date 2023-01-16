747aa047e82dc237d6ec842705abc48eaa3f770c
dawidmarek95x <dawid.marek95x@gmail.com>
2023-01-17 00:11:41 +0100

    The project has been set up and todo list component has been added

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TodoList from "./components/TodoList";
+
+const App: React.FC = () => {
+  const todos = [{ id: "t1", text: "Learn TypeScript" }];
+
+  return (
+    <div className="App">
+      <TodoList items={todos} />
+    </div>
+  )
+};
+
+export default App;
